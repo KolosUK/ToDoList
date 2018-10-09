bb60378a10b301101b9a0abcbeae9aa1bd57c513
Pavlo Didyk <pavlo.didyk@jetsoftpro.com>
2018-10-09 15:45:02 +0300

    Init project and base functionality

---
+++ ServiceApp/Models/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ServiceApp.Models
+{
+    public class ApplicationContext : DbContext
+    { 
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
