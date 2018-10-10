fa85df4a2d8c0b8c526c6149caa94c6e850fd4f5
Pavlo Didyk <pavlo.didyk@jetsoftpro.com>
2018-10-10 12:47:33 +0300

    Add backend validation and add new migration

---
+++ ServiceApp/Migrations/20181010092257_Change type Price in model Product.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ServiceApp.Migrations
+{
+    public partial class ChangetypePriceinmodelProduct : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Company",
+                table: "Products",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Products",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Company",
+                table: "Products",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 100);
+        }
+    }
+}
--- ServiceApp/Models/Product.cs
+++ ServiceApp/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -9,8 +10,16 @@ namespace ServiceApp.Models
 {
     public class Product { 
         public int Id { get; set; }
-        public string Name { get; set; } 
+        [Required]
+        [MinLength(3)]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(3)]
+        [StringLength(100)]
         public string Company { get; set; } 
-        public int Price { get; set; }
+        [Required]
+        [Range(0, 100000)]
+        public decimal Price { get; set; }
     }
 }
